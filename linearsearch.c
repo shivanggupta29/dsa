b1ca3974b8150de03b3f9b9e72e4a1fade1a201e
Shivang Gupta <shivang.gupta.cse.2025@miet.ac.in>
2026-08-24 22:08:58 +0530

    Add linear search

---
+++ linearsearch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, key, i;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(a[i] == key) {
+            printf("Element found at position %d", i + 1);
+            return 0;
+        }
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
